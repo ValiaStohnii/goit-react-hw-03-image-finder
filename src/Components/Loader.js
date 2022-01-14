eb208c2abb4044a518f00ae02b761cd6f1f51f52
ValiaStohnii <valia19961018@gmail.com>
2022-01-15 00:53:55 +0200

    fix loading

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { MutatingDots } from 'react-loader-spinner';
 
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <MutatingDots color="#00BFFF" height={80} width={80} />
+    </div>
+  );
 };
 export default Loader;
