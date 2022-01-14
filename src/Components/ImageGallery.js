fb7ddd04005e843ee38ba89f68ee58537c05d607
ValiaStohnii <valia19961018@gmail.com>
2022-01-15 00:05:03 +0200

    fetch

--- src/Components/ImageGallery.js
+++ src/Components/ImageGallery.js
@@ -4,35 +4,57 @@ import ImageGalleryItem from './ImageGalleryItem';
 class ImageGallery extends Component {
   state = {
     image: null,
-    loading: false,
+    error: null,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.imageName !== this.props.imageName) {
-      this.setState({ loading: true });
+      this.setState({ status: 'pending' });
 
       fetch(
         `https://pixabay.com/api/?key=24200424-23477fc3694bee0d0a7f46301&page=1&q=${this.props.imageName}&image_type=photo&orientation=horizontal&per_page=12`,
       )
-        .then(r => r.json())
-        .then(image => this.setState({ image }))
-        .finally(() => this.setState({ loading: false }));
+        .then(r => {
+          if (r.ok) {
+            return r.json();
+          }
+          return Promise.reject(new Error(`Please enter a more specific query`));
+        })
+        .then(image => {
+          this.setState({ image, status: 'resolved' });
+          console.log(image.hits);
+        })
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
   render() {
-    const { loading, image } = this.state;
-    const { imageName } = this.props;
-    return (
-      <div>
-        {loading && <div>Loading...</div>}
-        {!imageName && <div>Введіть назву картинки</div>}
-        {image && (
+    const { image, error, status } = this.state;
+
+    if (status === 'idle') {
+      return <div>You must enter query parameters!</div>;
+    }
+    if (status === 'pending') {
+      return <div>Loading...</div>;
+    }
+    if (status === 'rejected') {
+      return <h2>{error.message}</h2>;
+    }
+    if (status === 'resolved') {
+      return (
+        <div>
           <ul className="gallery">
-            <ImageGalleryItem imageName={image.tags} imageUrl={image.webformatURL} />
+            {image.hits.map(entry => (
+              <ImageGalleryItem
+                key={entry.id}
+                imageName={entry.tags}
+                imageUrl={entry.webformatURL}
+              />
+            ))}
           </ul>
-        )}
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
 
