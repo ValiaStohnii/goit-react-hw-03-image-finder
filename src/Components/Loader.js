e845e02a2a9ef5533a2a441e267de8f28d5f6763
ValiaStohnii <valia19961018@gmail.com>
2022-01-15 15:09:14 +0200

    add loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -4,7 +4,7 @@ import { MutatingDots } from 'react-loader-spinner';
 const Loader = () => {
   return (
     <div>
-      <MutatingDots color="#00BFFF" height={80} width={80} />
+      <MutatingDots height={100} color={'red'} width={100} />
     </div>
   );
 };
